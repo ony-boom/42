5126b73536e35030d5bd4f6e865a24ba35faffff
Ony <onyrakoto27@gmail.com>
2024-01-22 08:42:41 +0300

    add c04/ex03

---
+++ c/c04/ex03/ft_atoi.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rony-lov <rony-lov@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 07:33:50 by rony-lov          #+#    #+#             */
+/*   Updated: 2024/01/22 08:41:28 by rony-lov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*trim_start(char *str)
+{
+	while (*str && (*str == ' ' || *str == '0'))
+		str++;
+	return (str);
+}
+
+int	set_sign(char **input)
+{
+	int		count;
+	char	*str;
+
+	count = 0;
+	str = *input;
+	while (*str && (*str == '-' || *str == '+'))
+	{
+		if (*str == '-')
+			count++;
+		str++;
+	}
+	*input = str;
+	return (count % 2);
+}
+
+int	is_num(char c, int *c_as_int)
+{
+	int	as_int;
+	int	is_ok;
+
+	as_int = c - '0';
+	is_ok = as_int >= 0 && as_int <= 9;
+	if (is_ok)
+		*c_as_int = as_int;
+	return (is_ok);
+}
+
+int	ft_atoi(char *base_str)
+{
+	int		num;
+	char	*str;
+	int		str_n;
+	int		is_negative;
+
+	num = 0;
+	str = trim_start(base_str);
+	is_negative = set_sign(&str);
+	if (!*str || !is_num(*str, &str_n))
+		return (0);
+	num = *str - '0';
+	str++;
+	while (is_num(*str, &str_n))
+	{
+		num *= 10;
+		num += str_n;
+		str++;
+	}
+	if (is_negative)
+		num *= -1;
+	return (num);
+}
--- c/rush01/ex00/skyscraper.c
+++ c/rush01/ex00/skyscraper.c
@@ -6,42 +6,37 @@
 /*   By: rony-lov <rony-lov@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 08:55:36 by rony-lov          #+#    #+#             */
-/*   Updated: 2024/01/21 00:31:10 by rony-lov         ###   ########.fr       */
+/*   Updated: 2024/01/21 15:54:43 by rony-lov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	solve(int **map, int **map_params, int row, int col);
-int		**rejet_map(int **map);
-int		**init_map(int map_size);
-void	free_map(int **map, int map_size);
-void	print_map(int **map, int map_size);
-int		**parse_map_params(char *args, int **map_params, int map_size);
+void solve(int **map, int **map_params, int row, int col);
+int **rejet_map(int **map);
+int **init_map(int map_size);
+void free_map(int **map, int map_size);
+void print_map(int **map, int map_size);
+int **parse_map_params(char *args, int **map_params, int map_size);
 
-int	main(int argc, char *argv[])
-{
-	int			**map;
-	int			**map_params;
-	const int	map_size = 4;
-	int			row;
-	int			col;
+int main(int argc, char *argv[]) {
+  int **map;
+  int **map_params;
+  const int map_size = 4;
+  int row;
+  int col;
 
-	row = 0;
-	col = 0;
-	if (argc < 2)
-	{
-		return (0);
-	}
-	map = init_map(map_size);
-	map_params = init_map(map_size);
-	parse_map_params(argv[1], map_params, map_size);
-	rejet_map(map);
-	solve(map, map_params, row, col);
-	if (map[0][0] == 0)
-		write(1, "Error\n", 6);
-	else
-		print_map(map, map_size);
-	free_map(map, map_size);
-	free_map(map_params, map_size);
+  row = 0;
+  col = 0;
+  if (argc < 2) {
+    return (0);
+  }
+  map = init_map(map_size);
+  map_params = init_map(map_size);
+  parse_map_params(argv[1], map_params, map_size);
+  rejet_map(map);
+  solve(map, map_params, row, col);
+  print_map(map, map_size);
+  free_map(map, map_size);
+  free_map(map_params, map_size);
 }
