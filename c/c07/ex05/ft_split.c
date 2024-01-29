f9e1d7e3d91f60c87e1cb48b265830198541e777
Ony <onyrakoto27@gmail.com>
2024-01-29 15:45:33 +0300

    add ex07

--- c/c07/ex05/ft_split.c
+++ c/c07/ex05/ft_split.c
@@ -6,124 +6,91 @@
 /*   By: username <your@mail.com>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 19:31:58 by rony-lov          #+#    #+#             */
-/*   Updated: 2024/01/29 13:03:28 by username         ###   ########.fr       */
+/*   Updated: 2024/01/29 15:43:07 by username         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
+int	is_sep(char c, char *charset)
 {
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while (charset[i] != '\0')
+	{
+		if (c == charset[i])
+			return (1);
 		i++;
-	return (i);
-}
-
-int	is_sep(char *sep_like, char *sep)
-{
-	while (*sep)
-	{
-		if (*sep_like != *sep)
-			return (0);
-		sep++;
-		sep_like++;
 	}
-	return (1);
+	if (c == '\0')
+		return (1);
+	return (0);
 }
 
-int	get_size(const char *strr, char *charset)
+int	total_words(char *str, char *charset)
 {
-	int		size;
-	char	*str;
+	int	i;
+	int	words;
 
-	size = 0;
-	str = (char *)strr;
-	while (*str)
+	words = 0;
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (is_sep(str, charset))
-		{
-			str += ft_strlen(charset);
-			size++;
-			continue ;
-		}
-		str++;
+		if (is_sep(str[i + 1], charset) == 1 && is_sep(str[i],
+				charset) == 0)
+			words++;
+		i++;
 	}
-	size++;
-	return (size);
+	return (words);
 }
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
+void	copy_word(char *dest, char *src, char *charset)
 {
-	unsigned int	i;
+	int	i;
 
 	i = 0;
-	while (src[i] != '\0' && i < n)
+	while (is_sep(src[i], charset) == 0)
 	{
 		dest[i] = src[i];
-		++i;
-	}
-	while (i < n)
-	{
-		dest[i] = '\0';
 		i++;
 	}
-	return (dest);
+	dest[i] = '\0';
 }
 
-char	**ft_split(const char *str, char *charset)
+void	split_words(char **array, char *str, char *charset)
 {
-	int		i;
-	char	**strs;
-	char	*m_str;
-	char	*cp;
-	int		curr_size;
+	int	i;
+	int	j;
+	int	word;
 
+	word = 0;
 	i = 0;
-	m_str = (char *)str;
-	cp = (char *)str;
-	strs = malloc(sizeof(char *) * get_size(str, charset) + 1);
-	while (*m_str)
+	while (str[i] != '\0')
 	{
-		if (is_sep(m_str, charset))
-		{
-			m_str += ft_strlen(charset);
-			strs[i] = malloc(sizeof(char) * curr_size);
-			ft_strncpy(strs[i], cp, curr_size);
-			cp += curr_size + ft_strlen(charset);
-			curr_size = 0;
+		if (is_sep(str[i], charset) == 1)
 			i++;
+		else
+		{
+			j = 0;
+			while (is_sep(str[i + j], charset) == 0)
+				j++;
+			array[word] = (char *)malloc(sizeof(char) * (j + 1));
+			copy_word(array[word], str + i, charset);
+			i += j;
+			word++;
 		}
-		curr_size++;
-		m_str++;
 	}
-	if (i < get_size(str, charset))
-	{
-		strs[i] = malloc(sizeof(char) * curr_size);
-		ft_strncpy(strs[i], cp, curr_size);
-	}
-	strs[++i] = 0;
-	return (strs);
 }
 
-int	main(void)
+char	**ft_split(char *str, char *charset)
 {
-	char	**s;
-	int		n;
+	char	**array;
+	int		words;
 
-	s = ft_split("", " ");
-	n = 0;
-	while (s[n] != NULL)
-	{
-		n++;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		printf("%s\n", s[i]);
-		free(s[i]);
-	}
-	free(s);
+	words = total_words(str, charset);
+	array = (char **)malloc(sizeof(char *) * (words + 1));
+	array[words] = 0;
+	split_words(array, str, charset);
+	return (array);
 }
