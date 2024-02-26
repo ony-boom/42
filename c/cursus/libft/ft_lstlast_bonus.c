5158dc7676fc90913719a7432b2baa22ca6ed1cb
Ony <onyrakoto27@gmail.com>
2024-02-26 17:04:46 +0300

    bonus

--- c/cursus/libft/ft_lstlast_bonus.c
+++ c/cursus/libft/ft_lstlast_bonus.c
@@ -6,10 +6,24 @@
 /*   By: rony-lov <rony-lov@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 16:56:13 by rony-lov          #+#    #+#             */
-/*   Updated: 2024/02/26 16:56:27 by rony-lov         ###   ########.fr       */
+/*   Updated: 2024/02/26 17:04:28 by rony-lov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstlast(t_list *lst);
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*last;
+
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+	{
+		last = lst;
+		lst = lst->next;
+	}
+	if (lst->next)
+		last = lst->next;
+	return (last);
+}
