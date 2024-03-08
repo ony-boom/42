e5c1cfd5a9b51dd248036915fc2551040b2dd973
ony <onyrakoto27@gmail.com>
2024-03-08 21:26:08 +0300

    So I've changed many things there but quite don't remember all.

--- c/cursus/ft_printf/includes/ft_printf.h
+++ c/cursus/ft_printf/includes/ft_printf.h
@@ -6,54 +6,51 @@
 /*   By: rony-lov <rony-lov@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 13:15:35 by rony-lov          #+#    #+#             */
-/*   Updated: 2024/03/06 14:53:44 by rony-lov         ###   ########.fr       */
+/*   Updated: 2024/03/07 09:17:33 by rony-lov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#define FT_PRINTF_H
 
-# include "../libft/libft.h"
-# include <stdarg.h>
-# include <stdint.h>
+#include "../libft/libft.h"
+#include <stdarg.h>
+#include <stdint.h>
 
-typedef enum e_format
-{
-	CHAR = 'c',
-	INTEGER = 'i',
-	STR = 's',
-	DECIMAL = 'd',
-	POINTER = 'p',
-	UNSIGNED_INT = 'u',
-	HEX = 'x',
-	UPPER_HEX = 'X',
-	PERCENT = '%'
-}							t_format_specifier;
+typedef enum e_format {
+  CHAR = 'c',
+  INTEGER = 'i',
+  STR = 's',
+  DECIMAL = 'd',
+  POINTER = 'p',
+  UNSIGNED_INT = 'u',
+  HEX = 'x',
+  UPPER_HEX = 'X',
+  PERCENT = '%'
+} t_format_specifier;
 
-typedef struct format_modifier
-{
-	unsigned int			left_pad;
-	unsigned int			right_pad;
-	unsigned int			zero_pad;
-}							t_format_modifier;
 
-typedef struct format
-{
-	t_format_modifier		*modifier;
-	t_format_specifier		specifier;
-}							t_format;
+typedef struct format_modifier {
+	int len;
+	int is_right;
+	int with_zero;
+} t_format_modifier;
 
-typedef char				*(*t_format_handler)(va_list params);
+typedef struct format {
+  t_format_modifier modifier;
+  t_format_specifier specifier;
+} t_format;
 
-# define SPECIFER_LIST_SIZE 10
+typedef char *(*t_format_handler)(va_list params);
 
-int							ft_printf(const char *str, ...);
-int							get_hex(int n, int use_upper);
-t_format					*get_format(const char *str, int *format_len);
-t_format_modifier			*get_modifier(const char *str, int *modifier_len);
-void						free_format(t_format *format);
+#define SPECIFER_LIST_SIZE 10
 
-char						*new_str_builder(t_format *format, va_list params);
-const t_format_specifier	*get_specifier_list(void);
+int ft_printf(const char *str, ...);
+int get_hex(int n, int use_upper);
+t_format get_format(const char *str, int *format_len);
+t_format_modifier get_modifier(const char *str);
+
+char *new_str_builder(t_format format, va_list params);
+const t_format_specifier *get_specifier_list(void);
 
 #endif // FT_PRINTF_H
--- c/cursus/ft_printf/src/parser.c
+++ c/cursus/ft_printf/src/parser.c
@@ -6,7 +6,7 @@
 /*   By: rony-lov <rony-lov@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 21:05:38 by rony-lov          #+#    #+#             */
-/*   Updated: 2024/03/06 14:18:13 by rony-lov         ###   ########.fr       */
+/*   Updated: 2024/03/07 09:15:00 by rony-lov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,15 @@ t_format_specifier	get_specifier(const char c)
 	return (specifier);
 }
 
-t_format	*get_format(const char *str, int *format_len)
+t_format	get_format(const char *str, int *format_len)
 {
-	t_format	*format;
+	t_format	format;
 
-	format = malloc(sizeof(t_format));
-	if (!format)
-		return (NULL);
-	format->specifier = get_specifier(*str);
+	format.specifier = get_specifier(*str);
 	// at this point `str` is something like 👉️ s|d|x|i
 	// + the rest of the string
-	format->modifier = get_modifier(str++, format_len);
+	format.modifier = get_modifier(str++);
 	// at this point `str` like the above but without the specifier
-	*format_len += 1;
+	*format_len += format.modifier.len + 1;
 	return (format);
-}
-
-void	free_format(t_format *format)
-{
-	if (!format)
-		return ;
-	free(format->modifier);
-	free(format);
 }
