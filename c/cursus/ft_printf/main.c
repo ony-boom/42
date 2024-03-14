16c10f4cc21ca58850e88de76bc1d1b277906a33
ony <onyrakoto27@gmail.com>
2024-03-14 08:20:32 +0300

    shit, here we go again
    rewrite everything again, just because of non printable char

--- c/cursus/ft_printf/main.c
+++ c/cursus/ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: rony-lov <rony-lov@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 14:22:16 by rony-lov          #+#    #+#             */
-/*   Updated: 2024/03/13 07:42:48 by rony-lov         ###   ########.fr       */
+/*   Updated: 2024/03/14 08:00:13 by rony-lov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 // TODO: fix char printer on 0
 int	main(void)
 {
-	char *str = " %c %c %c \n";
+	char *str = "%c\n";
 	
-	ft_printf(str, '0', 0, 1);
-	printf(str, '0', 0, '1');
+	ft_printf(str, 0);
+	printf(str, 0);
 }
