02298f483620d355ee3bb7936bbbffb992722f9b
Ony <onyrakoto27@gmail.com>
2024-03-16 10:46:31 +0300

    make normal better

--- c/cursus/ft_printf/includes/ft_printf.h
+++ c/cursus/ft_printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rony-lov <rony-lov@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 13:15:35 by rony-lov          #+#    #+#             */
-/*   Updated: 2024/03/05 19:41:29 by rony-lov         ###   ########.fr       */
+/*   Updated: 2024/03/16 10:41:10 by rony-lov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,47 +28,49 @@ typedef enum e_format
 	HEX = 'x',
 	UPPER_HEX = 'X',
 	PERCENT = '%'
-}				t_format;
+}					t_format;
 
 # define PRINTER_LIST_SIZE 10
 
-typedef int		(*t_print_fn)(va_list);
+typedef int			(*t_print_fn)(va_list);
 
 typedef struct s_printer
 {
-	t_format	format_specifier;
-	t_print_fn	fn;
-}				t_printer;
+	t_format		format_specifier;
+	t_print_fn		fn;
+}					t_printer;
 
-typedef struct format_config {
-	t_format format;
-	unsigned int min_width;
-	unsigned int left_pad;
-	unsigned int right_pad;
-} t_format_config;
+typedef struct format_config
+{
+	t_format		format;
+	unsigned int	min_width;
+	unsigned int	left_pad;
+	unsigned int	right_pad;
+}					t_format_config;
 
-t_printer		*printer_new(t_format specifier, t_print_fn printer_fn);
+t_printer			*printer_new(t_format specifier, t_print_fn printer_fn);
 
-t_printer		**init_printers(void);
-void			register_printer(t_printer **registry, t_printer *printer);
+t_printer			**init_printers(void);
+void				register_printer(t_printer **registry, t_printer *printer);
 
-t_printer		*get_printer_at_format(t_printer **printers, t_format format);
+t_printer			*get_printer_at_format(t_printer **printers,
+						t_format format);
 
-void			free_mem(t_printer **registry);
+void				free_mem(t_printer **registry);
 
-int				ft_printf(const char *format, ...);
+int					ft_printf(const char *format, ...);
 
-t_printer		*int_printer(void);
-t_printer		*char_printer(void);
-t_printer		*str_printer(void);
-t_printer		*decimal_printer(void);
-t_printer		*unsignedint_printer(void);
+t_printer			*int_printer(void);
+t_printer			*char_printer(void);
+t_printer			*str_printer(void);
+t_printer			*decimal_printer(void);
+t_printer			*unsignedint_printer(void);
 
-int	get_hex(int n, int use_upper);
-int				hex_base_printf(va_list params, int uppper);
-t_printer		*hex_lower_printer(void);
-t_printer		*hex_upper_printer(void);
-t_printer		*percent_printer(void);
-t_printer		*pointer_printer(void);
+int					get_hex(int n, int use_upper);
+int					hex_base_printf(va_list params, int uppper);
+t_printer			*hex_lower_printer(void);
+t_printer			*hex_upper_printer(void);
+t_printer			*percent_printer(void);
+t_printer			*pointer_printer(void);
 
 #endif // FT_PRINTF_H
--- c/cursus/ft_printf/libft/ft_isnum.c
+++ c/cursus/ft_printf/libft/ft_isnum.c
@@ -6,7 +6,7 @@
 /*   By: rony-lov <rony-lov@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 11:32:07 by rony-lov          #+#    #+#             */
-/*   Updated: 2024/02/24 11:32:24 by rony-lov         ###   ########.fr       */
+/*   Updated: 2024/03/16 10:35:39 by rony-lov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ int	ft_isnum(char c, int *c_as_int)
 	int	is_ok;
 
 	as_int = c - '0';
-	is_ok = as_int >= 0 && as_int <= 9;
+	is_ok = 0;
+	if (as_int >= 0 && as_int <= 9)
+		is_ok = 1;
 	if (is_ok)
 		*c_as_int = as_int;
 	return (is_ok);
