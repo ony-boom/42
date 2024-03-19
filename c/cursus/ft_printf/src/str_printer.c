f7653ff5c56acc33bc9ef9c7d916f13ee7938b82
ony <onyrakoto27@gmail.com>
2024-03-19 22:05:19 +0300

    fix other stuff tomorow

--- c/cursus/ft_printf/src/str_printer.c
+++ c/cursus/ft_printf/src/str_printer.c
@@ -1,47 +1,54 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   char_printer.c                                     :+:      :+:    :+:   */
+/*   str_printer.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rony-lov <rony-lov@student.42antananarivo  +#+  +:+       +#+        */
+/*   By: rony-lov <rony-lov@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/19 06:35:56 by rony-lov          #+#    #+#             */
-/*   Updated: 2024/03/19 21:46:15 by rony-lov         ###   ########.fr       */
+/*   Created: 2024/03/19 21:36:57 by rony-lov          #+#    #+#             */
+/*   Updated: 2024/03/19 22:04:00 by rony-lov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-int	char_print_fn(void *character)
+static int str_printer(char *str)
 {
-	char	c;
-
-	c = *(char *)character;
-	return (ft_putchar_fd(c, 1));
+	if (!str || !*str)
+	{
+		if (!str)
+			return (ft_putstr_fd("(null)", 1));
+		return (0);
+	}
+	return (ft_putstr_fd(str, 1));
 }
 
-int	print_char(char c, t_format_config config)
+static int	str_print_fn(void *character)
+{
+	char	*str;
+
+	str = (char *)character;
+	return (str_printer(str));
+}
+
+int	print_str(char *str, t_format_config config)
 {
 	int							pad;
 	int							printed;
-	char						padding;
 	t_format_modifier_config	modifier;
 
 	printed = 0;
-	padding = ' ';
 	modifier = config.modifier_config;
-	pad = modifier.pad.len - 1;
+	pad = modifier.pad.len - ft_strlen(str);
 	if (!config.has_config || pad <= 0)
-		return (ft_putchar_fd(c, 1));
-	if (modifier.pad.is_zero)
-		padding = '0';
+		return (str_printer(str));
 	printed += print_pad(
 		(t_print_pad_params){
-			.rest = &c,
+			.rest = str,
 			.padding_count = pad,
-			.padding_char = padding,
+			.padding_char = ' ',
 			.is_right = modifier.pad.is_right,
 		},
-		char_print_fn);
+		str_print_fn);
 	return (printed);
 }
