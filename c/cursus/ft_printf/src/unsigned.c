8e2d8760c48eda0b661d91b163a723fbda13bbc4
Ony <orakotonirina@bocasay.com>
2024-04-09 06:34:27 +0300

    add unsigned

---
+++ c/cursus/ft_printf/src/unsigned.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unsigned.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rony-lov <rony-lov@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 22:27:29 by rony-lov          #+#    #+#             */
+/*   Updated: 2024/04/08 22:28:49 by rony-lov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_printf.h"
+
+static int	unsigned_printer(unsigned int n)
+{
+	int	printed;
+
+	printed = 0;
+	if (n >= 10)
+	{
+		printed += unsigned_printer((n / 10));
+		printed += unsigned_printer((n % 10));
+	}
+	else
+		printed += ft_putchar_fd(n + '0', 1);
+	return (printed);
+}
